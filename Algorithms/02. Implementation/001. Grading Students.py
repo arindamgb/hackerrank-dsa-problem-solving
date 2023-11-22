368cf63dfb815582eed23ee975d8de391b8a78db
Arindam Gustavo Biswas <arindamgb@gmail.com>
2023-11-23 02:42:42 +0600

    Added gradingStudents 23.11.23

---
+++ Algorithms/02. Implementation/001. Grading Students.py
@@ -0,0 +1,26 @@
+
+
+def gradingStudents(grades):
+    # Write your code here
+    updatedGrades = []
+    for grade in grades:
+        if grade >= 38:
+            bar = ((grade // 5) + 1) * 5
+            if (bar - grade) < 3:
+                #print(bar)
+                updatedGrades.append(int(bar))
+            else:
+                #print(grade)
+                updatedGrades.append(int(grade))
+        else:
+            #print(grade)
+            updatedGrades.append(int(grade))
+
+    return updatedGrades
+
+
+grades = [73, 67, 38, 33]
+
+afterUpdate = gradingStudents(grades)
+
+print(afterUpdate)
