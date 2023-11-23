bc8c3afd8aaf774ba201cf81e113bba1c4de46be
Arindam Gustavo Biswas <arindamgb@gmail.com>
2023-11-23 17:16:24 +0600

    Added Arrays - DS

---
+++ Data Structures/01. Arrays/001. Arrays - DS.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/arrays-ds/problem
+
+def reverseArray(a):
+    # Write your code here
+    a.reverse()
+    return a
+
+
+arr = [1, 2, 3]
+res = reverseArray(arr)
+print(res)
